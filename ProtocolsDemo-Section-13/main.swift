b5a6ee7efaceec2b5722bcd682e70a5d519fdd58
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-05 11:57:16 +0300

    Shows protocols and gives information on it

--- ProtocolsDemo-Section-13/main.swift
+++ ProtocolsDemo-Section-13/main.swift
@@ -1,11 +1,51 @@
-//
-//  main.swift
-//  ProtocolsDemo-Section-13
-//
-//  Created by Abdurahman on 4.11.2023.
-//
+protocol CanFly {
+    func fly()
+}
 
-import Foundation
+class Bird {
+    var isFemale = true
+    
+    func layEgg(){
+        if(isFemale){
+            print("The bird makes a new bird in a shell")
+        }
+    }
+}
 
-print("Hello, World!")
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky")
+    }
+    
+    func soar(){
+        print("The eagle glides in the air using air currents")
+    }
+}
+
+class Penguin: Bird {
+    func swim(){
+        print("The penguin paddles through the water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly){
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lift off into the air")
+    }
+}
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let myPlane = Airplane()
+myEagle.fly()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
+
 
